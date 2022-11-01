cd93503d33531fd421748b557fec36527348a4bf
NjaraMcl <69254013+NjaraMcl@users.noreply.github.com>
2022-11-01 18:00:01 +0300

    collectstatic

--- staticfiles/nj/js/NjMain.js
+++ staticfiles/nj/js/NjMain.js
@@ -1,9 +1,13 @@
 
 var sidebar = document.getElementsByClassName('nj-sidebar'),
     mainHeader = document.getElementsByClassName('nj-header'),
-    mobileNavTrigger = document.getElementsByClassName('nj-header-trigger');
+    mobileNavTrigger = document.getElementsByClassName('nj-header-trigger'),
+    submenu = document.getElementsByClassName('has_child');
 
 function toggleSidebar(ref) {
     document.getElementById("nj-header-trigger").classList.toggle('change');
     document.getElementById("nj-sidebar").classList.toggle('is-visible');
+}
+function myFunction(ref) {
+    document.getElementById("nj-sub-side-list").classList.toggle('is_visible');
 }
