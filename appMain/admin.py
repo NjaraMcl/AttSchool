a6a42cd9b75c7b535e6af4127e18a747ffd9b9ab
NjaraMcl <69254013+NjaraMcl@users.noreply.github.com>
2022-11-19 15:06:27 +0300

    eleveadmin

--- appMain/admin.py
+++ appMain/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import Eleve, Classe, Teacher, Presence
 
 
-admin.site.register(Eleve)
+class EleveAdmin(admin.ModelAdmin):
+    list_display = (
+        "nom",
+        "prenom",
+        "gender",
+        "class_id",
+    )
+
+
+admin.site.register(Eleve, EleveAdmin)
 admin.site.register(Classe)
 admin.site.register(Teacher)
 admin.site.register(Presence)
